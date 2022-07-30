84851722d2c9dc8cc9589ce61fb1e73dbacebfec
lzanelzaz <lzanelzaz@gmail.com>
2022-07-30 10:37:02 +0700

    top app bar navigation

--- app/src/main/java/ru/lzanelzaz/icerock_test_task/ListRepositoriesFragment.kt
+++ app/src/main/java/ru/lzanelzaz/icerock_test_task/ListRepositoriesFragment.kt
@@ -3,12 +3,11 @@ package ru.lzanelzaz.icerock_test_task
 import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.lzanelzaz.icerock_test_task.databinding.FragmentListRepositoriesBinding
 import javax.inject.Inject
@@ -18,12 +17,15 @@ class ListRepositoriesFragment : Fragment() {
 
     lateinit var viewModel: RepositoriesListViewModel
 
+    lateinit var binding : FragmentListRepositoriesBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewModel = RepositoriesListViewModel()
-        val binding = FragmentListRepositoriesBinding.inflate(inflater, container, false)
+        binding = FragmentListRepositoriesBinding.inflate(inflater, container, false)
+
 //listOf(Repo("repo", "descr", "Kotlin"))
 
         viewModel.state.observe(
@@ -54,6 +56,26 @@ class ListRepositoriesFragment : Fragment() {
 
         binding.listRepositoriesRecyclerView.setHasFixedSize(true)
 
+
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.topAppBar.setNavigationOnClickListener {
+            it.findNavController().navigate(R.id.action_listRepositoriesFragment_to_authFragment)
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.log_out -> {
+                    view.findNavController().navigate(R.id.action_listRepositoriesFragment_to_authFragment)
+                    true
+                }
+                else -> false
+            }
+
+        }
     }
 }

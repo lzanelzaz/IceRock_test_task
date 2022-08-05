98497bd2973e2dc32aa30f0af1a4a98516a1b83c
lzanelzaz <lzanelzaz@gmail.com>
2022-08-05 13:41:34 +0700

    Hilt network module

--- app/src/main/java/ru/lzanelzaz/icerock_test_task/AppRepository.kt
+++ app/src/main/java/ru/lzanelzaz/icerock_test_task/AppRepository.kt
@@ -1,10 +1,5 @@
 package ru.lzanelzaz.icerock_test_task
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
@@ -15,32 +10,23 @@ import javax.inject.Inject
 
 class AppRepository @Inject constructor() {
     suspend fun getRepositories(): List<Repo> {
-        return GithubApi.retrofitService.getRepositories("token ${KeyValueStorage.authToken}")
+        return NetworkModule.provideGithubApiService().getRepositories("token ${KeyValueStorage.authToken}")
     }
 
     suspend fun getRepository(repoId: String): RepoDetails {
-        return GithubApi.retrofitService.getRepository("token ${KeyValueStorage.authToken}", repoId)
+        return NetworkModule.provideGithubApiService().getRepository("token ${KeyValueStorage.authToken}", repoId)
     }
 
     suspend fun getRepositoryReadme(ownerName: String, repositoryName: String, branchName: String): String {
-        return GithubRawUserContent.retrofitService.getRepositoryReadme(ownerName, repositoryName, branchName)
+        return NetworkModule.provideGithubRawUserContentService().getRepositoryReadme(ownerName, repositoryName, branchName)
     }
 
     suspend fun signIn(token: String) : UserInfo {
         KeyValueStorage.authToken = token
-        return GithubApi.retrofitService.signIn("token ${KeyValueStorage.authToken}")
+        return NetworkModule.provideGithubApiService().signIn("token ${KeyValueStorage.authToken}")
     }
+
 }
-
-private val json = Json {
-    ignoreUnknownKeys = true
-}
-
-private val apiGithub = Retrofit.Builder()
-    .baseUrl("https://api.github.com")
-    .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-    .build()
-
 interface GithubApiService {
     @GET("user")
     suspend fun signIn(@Header("Authorization") token: String): UserInfo
@@ -58,17 +44,6 @@ interface GithubApiService {
     ): RepoDetails
 }
 
-object GithubApi {
-    val retrofitService: GithubApiService by lazy {
-        apiGithub.create(GithubApiService::class.java)
-    }
-}
-
-private val rawGithubUserContent = Retrofit.Builder()
-    .baseUrl("https://raw.githubusercontent.com")
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .build()
-
 interface GithubRawUserContentService {
     @GET("{ownerName}/{repositoryName}/{branchName}/README.md")
     suspend fun getRepositoryReadme(
@@ -76,10 +51,4 @@ interface GithubRawUserContentService {
         @Path("repositoryName") repositoryName: String,
         @Path("branchName") branchName: String
     ): String
-}
-
-object GithubRawUserContent {
-    val retrofitService: GithubRawUserContentService by lazy {
-        rawGithubUserContent.create(GithubRawUserContentService::class.java)
-    }
 }
---
+++ app/src/main/java/ru/lzanelzaz/icerock_test_task/NetworkModule.kt
@@ -0,0 +1,30 @@
+package ru.lzanelzaz.icerock_test_task
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideGithubApiService(): GithubApiService = Retrofit.Builder()
+        .baseUrl("https://api.github.com")
+        .addConverterFactory(Json {ignoreUnknownKeys = true}.asConverterFactory("application/json".toMediaType()))
+        .build().create(GithubApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideGithubRawUserContentService(): GithubRawUserContentService = Retrofit.Builder()
+        .baseUrl("https://raw.githubusercontent.com")
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build().create(GithubRawUserContentService::class.java)
+}
